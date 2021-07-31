4dc6785152b025b4f05bacd5cd72eae077e3603b
kmrrahul37 <kmrrahul37@gmail.com>
2021-07-31 23:50:32 +0530

    First web file.

---
+++ module1-solution/assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,66 @@
+// (function () {
+//   'use strict';
+//   angular.module('LunchCheck',[])
+//   .controller('LunchCheckController',LunchCheckController);
+//   LunchCheckController.$inject=['$scope'];
+//   function LunchCheckController($scope) {
+//     $scope.dishes='';
+//     $scope.message='';
+//     $scope.checked=false;
+//
+//     $scope.checkLunch= function () {
+//       if ($scope.dishes.trim().length===0) {
+//         $scope.empty=true;
+//       }else {
+//         $scope.checked=true;
+//         $scope.empty=false;
+//         var arrayDishes=$scope.dishes.split(',');
+//         var arrayDisheWithoutEmptys=arrayDishes.filter(function (v) {
+//           return v.trim() !=='';
+//         });
+//         if (arrayDisheWithoutEmptys.length <=3) {
+//           $scope.message='Enjoy!';
+//         }else {
+//           $scope.message='Too much!';
+//         }
+//       }
+//     };
+//   }
+// })();
+
+
+// Code By Rahul kumar
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+      .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+      $scope.dishes = '';
+      $scope.message = '';
+      $scope.checked = false;
+
+      $scope.checkLunch = function() {
+          if ($scope.dishes.trim().length === 0) {
+              $scope.empty = true;
+          } else {
+              $scope.checked = true;
+              $scope.empty = false;
+              var arrayDishes = $scope.dishes.split(',');
+              var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                  return v.trim() !== '';
+              });
+
+              if (arrayDishesWithoutEmptys.length <= 3) {
+                  $scope.message = 'Enjoy!';
+              } else {
+                  $scope.message = 'Too much!';
+              }
+          }
+      };
+  }
+})();
+// Code By Rahul kumar
